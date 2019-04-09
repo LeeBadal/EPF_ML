724d39800e2b0e28006848317096106d6b7f1e28
Lee Badal <badal@kth.se>
2019-04-09 19:22:26 +0200

    -

--- model_predict.py
+++ model_predict.py
@@ -1,18 +1,75 @@
 import tensorflow as tf
+from rnn_elec import preprocess
+from rnn_elec import testing_data,seq_len
+import numpy as np
+import pandas as pd
+from collections import deque
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.models import Sequential
+import random
+
+def main():
+
+    categories = ["UP", "DOWN"]
 
 
-categories = ["UP", "DOWN"]
+    predict_x,predict_y = preprocesstest(testing_data)
+    modelpath = "models/RNN_Final-01-0.5189.model"
 
 
-def prepare(filepath):
+    model = tf.keras.models.load_model(modelpath)
+
+    print(model.metrics_names)
+    print(testing_data.head(52))
+    print(testing_data.tail())
 
 
-modelpath = "models/RNN_Final-10-0.668.model"
+    predict_loss,predict_accuracy = model.evaluate(predict_x,predict_y,verbose=0)
+    predict_single = model.predict(predict_x)
+
+    # i= 0
+    # while i < len(prediction):
+    #     print(prediction[i])
+    #     i +=1
+
+    print(predict_loss,predict_accuracy)
+    print(predict_single[0])
+    print(predict_single[1])
+    print(predict_single[2])
+    print(predict_single[3])
+    print(predict_single[4])
+    print(predict_single[5])
 
 
-model = tf.keras.models.load_model(modelpath)
 
 
-prediction = model.predict(X,verbose=0)
 
-print(prediction)
+def preprocesstest(df):
+    seq_data = []
+    prev_days = deque(maxlen=seq_len)
+    for i in df.values:
+        prev_days.append([n for n in i[:-1]])
+        if len(prev_days)==seq_len:
+            seq_data.append([np.array(prev_days),i[-1]])
+
+    list = []
+    ### balancing data
+    for seq,target in seq_data:
+        #print(seq,target)
+        if target == "DOWN":
+            list.append([seq,0])
+        elif target == "UP":
+            list.append([seq,1])
+
+    x= []
+    y = []
+    for seq,target in list:
+        x.append(seq)
+        y.append(target)
+
+    return np.array(x),np.array(y)
+
+
+
+if __name__ == "__main__":
+    main()
--- rnn_elec.py
+++ rnn_elec.py
@@ -13,9 +13,8 @@ from tensorflow.python.keras.callbacks import TensorBoard,ModelCheckpoint
 # TO RUN TENSORBOARD: cd /afs/kth.se/home/b/a/badal/.local/lib/python3.5/site-packages/tensorboard
 # then: python3 main.py --logdir=/afs/kth.se/home/b/a/badal/EPF_ML/logs
 
-pd.set_option('display.max_columns', 30)
-data = pd.read_csv("electricity-normalized.csv")
-seq_len = 150
+
+seq_len = 100
 future_pred = 48
 ratio_pred = "nsw"
 EPOCHS = 10
@@ -24,12 +23,90 @@ VAL_PERCENT = 15
 LEARNING_RATE = 0.001
 columns = ["date","period","nswprice","nswdemand","vicprice","vicdemand","class"]
 NAME = "{}-SEQ-{}-EPOCHS-{}-BATCH_SIZE-{}-LOSS{}-VAL_PERCENT{}".format(seq_len,EPOCHS,BATCH_SIZE,int(time.time()),LEARNING_RATE,VAL_PERCENT)
-data = data[["date","period","nswprice","nswdemand","class"]]
+pd.set_option('display.max_columns', 30)
+data = pd.read_csv("electricity-normalized.csv")
+data = data[["nswprice","class"]]
+
+TEST_PERCENT = 20
+
+times = sorted(data.index.values)
+
+testing_perc = times[-int((TEST_PERCENT/100)*len(times))]
+testing_data =data[(data.index >= testing_perc)]
 
 
+## redefine data for use in models
+
+data = data[(data.index < testing_perc)]
+
+## out of sample -> instead of true forwarding since we have historic data
+# we use out of sample forwarding to test our model, needed for time series forecasting to not overfit
+
+
+times = sorted(data.index.values)
+
+last_valper = times[-int((VAL_PERCENT/100)*len(times))] ## all values above this number are the last 5%
+
+validation_data = data[(data.index >= last_valper)] ## validation is a list of the last 5% of data
+main_data = data[(data.index < last_valper)]
+
+
+def main():
+
+
+
+
+
+    print(main_data.head())
+    print(validation_data.head())
+
+
+    train_x,train_y = preprocess(main_data)
+    validation_x,validation_y = preprocess(validation_data)
+
+    print("train data: {} validation: {}".format(len(train_x),len(validation_x)))
+
+    fixshape = train_x.shape[1]
+
+    model = Sequential()
+
+    model.add(CuDNNLSTM(128,input_shape=(train_x.shape[1:]), return_sequences = True))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+
+    model.add(CuDNNLSTM(128,input_shape=(train_x.shape[1:]),return_sequences = True))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+
+    model.add(CuDNNLSTM(128,input_shape=(train_x.shape[1:])))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+
+    model.add(Dense(32, activation ='relu'))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(2,activation="softmax"))
+
+
+    opt = tf.keras.optimizers.Adam(lr=LEARNING_RATE,decay=1e-6)
+
+    model.compile(loss='sparse_categorical_crossentropy',optimizer = opt,metrics=['accuracy'])
+
+    tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+
+    filepath = "RNN_Final-{epoch:02d}-{val_acc:.4f}"
+
+    checkpoint = ModelCheckpoint("models/{}.model".format(filepath,monitor='val_acc',verbose=1,save_best_only=True,mode='max'))
+
+    history = model.fit(
+    train_x,train_y,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=(validation_x,validation_y),
+    callbacks=[tensorboard,checkpoint]
+    )
 
 def preprocess(df):
-
     seq_data = []
     prev_days = deque(maxlen=seq_len)
     for i in df.values:
@@ -69,62 +146,6 @@ def preprocess(df):
 
     return np.array(x),y
 
-## out of sample -> instead of true forwarding since we have historic data
-# we use out of sample forwarding to test our model, needed for time series forecasting to not overfit
 
-
-times = sorted(data.index.values)
-
-last_5pct = times[-int((VAL_PERCENT/100)*len(times))] ## all values above this number are the last 5%
-
-validation_data = data[(data.index >= last_5pct)] ## validation is a list of the last 5% of data
-main_data = data[(data.index < last_5pct)]
-
-print(main_data.head())
-print(validation_data.head())
-
-
-train_x,train_y = preprocess(main_data)
-validation_x,validation_y = preprocess(validation_data)
-
-print("train data: {} validation: {}".format(len(train_x),len(validation_x)))
-
-fixshape = train_x.shape[1]
-
-model = Sequential()
-
-model.add(CuDNNLSTM(128,input_shape=(train_x.shape[1:]), return_sequences = True))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
-
-model.add(CuDNNLSTM(128,input_shape=(train_x.shape[1:]),return_sequences = True))
-model.add(Dropout(0.1))
-model.add(BatchNormalization())
-
-model.add(CuDNNLSTM(128,input_shape=(train_x.shape[1:])))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
-
-model.add(Dense(32, activation ='relu'))
-model.add(Dropout(0.2))
-
-model.add(Dense(2,activation="softmax"))
-
-
-opt = tf.keras.optimizers.Adam(lr=LEARNING_RATE,decay=1e-6)
-
-model.compile(loss='sparse_categorical_crossentropy',optimizer = opt,metrics=['accuracy'])
-
-tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
-
-filepath = "RNN_Final-{epoch:02d}-{val_acc:.3f}"
-
-checkpoint = ModelCheckpoint("models/{}.model".format(filepath,monitor='val_acc',verbose=1,save_best_only=True,mode='max'))
-
-history = model.fit(
-train_x,train_y,
-batch_size=BATCH_SIZE,
-epochs=EPOCHS,
-validation_data=(validation_x,validation_y),
-callbacks=[tensorboard,checkpoint]
-)
+if __name__ == "__main__":
+    main()
